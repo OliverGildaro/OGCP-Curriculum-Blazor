7509e31417b0f7ffa1438a93aaa789c5d49fa26c
Oliver.Gildaro <ocastro@fmsystems.com>
2024-12-16 15:44:42 -0400

    save

---
+++ src/Server/OGCP.Curriculums.BlazorServer/Components/controls/ButtonCmpBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OGCP.Curriculums.BlazorServer.Components.controls;
+public class ButtonCmpBase : ComponentBase
+{
+    protected string CssClass { get; set; } = "btn-primary";
+
+    protected void buttonHandleClick()
+    {
+        CssClass = "btn-secondary";
+        Console.WriteLine("Clcik");
+        //StateHasChanged(); for indirect state updates
+    }
+}
